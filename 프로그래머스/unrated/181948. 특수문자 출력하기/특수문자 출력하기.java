8cde3195903413536ac999bd09d8d3149cf00230
tngns1129 <57630359+tngns1129@users.noreply.github.com>
2023-05-20 12:41:38 +0900

    [unrated] Title: 특수문자 출력하기, Time: 115.85 ms, Memory: 64.8 MB -BaekjoonHub

--- 프로그래머스/unrated/181948. 특수문자 출력하기/특수문자 출력하기.java
+++ 프로그래머스/unrated/181948. 특수문자 출력하기/특수문자 출력하기.java
@@ -2,24 +2,6 @@ import java.util.Scanner;
 
 public class Solution {
     public static void main(String[] args) {
-        String s = "";
-        s = s + (char)(33);
-        s = s + (char)(64);
-        s = s + (char)(35);
-        s = s + (char)(36);
-        s = s + (char)(37);
-        s = s + (char)(94);
-        s = s + (char)(38);
-        s = s + (char)(42);
-        s = s + (char)(40);
-        s = s + (char)(92);
-        s = s + (char)(39);
-        s = s + (char)(34);
-        s = s + (char)(60);
-        s = s + (char)(62);
-        s = s + (char)(63);
-        s = s + (char)(58);
-        s = s + (char)(59);
-        System.out.println(s);
+        System.out.println("!@#$%^&*(\\'\"<>?:;");
     }
 }
