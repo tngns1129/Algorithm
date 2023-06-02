6979fca9d17417250be9a5aeb46f3c8754961305
tngns1129 <57630359+tngns1129@users.noreply.github.com>
2023-06-02 21:58:49 +0900

    [unrated] Title: 마지막 두 원소, Time: 0.01 ms, Memory: 65 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181927. 마지막 두 원소/마지막 두 원소.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int[] solution(int[] num_list) {
+        int[] answer = new int [num_list.length+1];
+        for(int i = 0; i < answer.length-1; i++){
+            answer[i] = num_list[i];
+        }
+        if(answer[answer.length-2]>answer[answer.length-3]){
+            answer[answer.length-1] = answer[answer.length-2]-answer[answer.length-3];
+        } else {
+            answer[answer.length-1] = answer[answer.length-2] * 2;
+        }
+        return answer;
+    }
+}
