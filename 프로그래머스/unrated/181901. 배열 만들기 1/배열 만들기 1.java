86160dccd991c49d31648f286aeb1542ef2db2f2
tngns1129 <57630359+tngns1129@users.noreply.github.com>
2023-06-11 19:01:43 +0900

    [unrated] Title: 배열 만들기 1, Time: 1.55 ms, Memory: 80.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181901. 배열 만들기 1/배열 만들기 1.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+
+class Solution {
+    public int[] solution(int n, int k) {
+        ArrayList <Integer> list = new ArrayList <Integer>();
+        for(int i = 1; i <= n; i++){
+            if(i%k == 0){
+                list.add(i);
+            }
+        }
+        int[] answer = new int[list.size()];
+        for(int i = 0; i < list.size(); i++){
+            answer[i] = list.get(i);
+        }
+        return answer;
+    }
+}
