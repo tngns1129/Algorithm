fe0bca286ae0d789d7cfe1847af9e96a1d25bc2d
tngns1129 <57630359+tngns1129@users.noreply.github.com>
2023-06-14 23:13:17 +0900

    [unrated] Title: 순서 바꾸기, Time: 0.02 ms, Memory: 81.8 MB -BaekjoonHub

--- 프로그래머스/unrated/181891. 순서 바꾸기/순서 바꾸기.java
+++ 프로그래머스/unrated/181891. 순서 바꾸기/순서 바꾸기.java
@@ -1,21 +1,30 @@
 class Solution {
     public int[] solution(int[] num_list, int n) {
         int[] answer = new int [num_list.length];
-        int[] left = new int[n];
-        int[] right = new int[num_list.length - n];
-        for(int i = 0; i < n; i++){
-            left[i] = num_list[i];
-        }
-        for(int i = n; i < num_list.length; i++){
-            right[i-n] = num_list[i];
-        }
-        for(int i = 0; i < answer.length; i++){
-            if(i < num_list.length - n){
-                answer[i] = right[i];
-            } else if (i >= num_list.length - n){
-                answer[i] = left[i-num_list.length+n];
+        // int[] left = new int[n];
+        // int[] right = new int[num_list.length - n];
+        // for(int i = 0; i < n; i++){
+        //     left[i] = num_list[i];
+        // }
+        // for(int i = n; i < num_list.length; i++){
+        //     right[i-n] = num_list[i];
+        // }
+        // for(int i = 0; i < answer.length; i++){
+        //     if(i < num_list.length - n){
+        //         answer[i] = right[i];
+        //     } else if (i >= num_list.length - n){
+        //         answer[i] = left[i-num_list.length+n];
+        //     }
+        // }
+        
+        for(int i = n; i < num_list.length + n; i++){
+            if(i > num_list.length-1){
+                answer[i-n] = num_list[i%num_list.length];
+            } else{
+                answer[i-n] = num_list[i];
             }
         }
+        
         return answer;
     }
 }
