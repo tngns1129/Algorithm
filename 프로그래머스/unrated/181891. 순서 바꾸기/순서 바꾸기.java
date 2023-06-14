5d1a8fe793ae08fab749beb328b522245a25eb23
tngns1129 <57630359+tngns1129@users.noreply.github.com>
2023-06-14 23:14:25 +0900

    [unrated] Title: 순서 바꾸기, Time: 0.03 ms, Memory: 68.7 MB -BaekjoonHub

--- 프로그래머스/unrated/181891. 순서 바꾸기/순서 바꾸기.java
+++ 프로그래머스/unrated/181891. 순서 바꾸기/순서 바꾸기.java
@@ -1,22 +1,7 @@
 class Solution {
     public int[] solution(int[] num_list, int n) {
-        int[] answer = new int [num_list.length];
-        // int[] left = new int[n];
-        // int[] right = new int[num_list.length - n];
-        // for(int i = 0; i < n; i++){
-        //     left[i] = num_list[i];
-        // }
-        // for(int i = n; i < num_list.length; i++){
-        //     right[i-n] = num_list[i];
-        // }
-        // for(int i = 0; i < answer.length; i++){
-        //     if(i < num_list.length - n){
-        //         answer[i] = right[i];
-        //     } else if (i >= num_list.length - n){
-        //         answer[i] = left[i-num_list.length+n];
-        //     }
-        // }
         
+        int[] answer = new int [num_list.length];
         for(int i = n; i < num_list.length + n; i++){
             if(i > num_list.length-1){
                 answer[i-n] = num_list[i%num_list.length];
@@ -24,7 +9,6 @@ class Solution {
                 answer[i-n] = num_list[i];
             }
         }
-        
         return answer;
     }
 }
