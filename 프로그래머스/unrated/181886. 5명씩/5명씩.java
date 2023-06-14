f2370f9589cf9a5e3a22ac1a4614873619d4c7ef
tngns1129 <57630359+tngns1129@users.noreply.github.com>
2023-06-15 00:15:47 +0900

    [unrated] Title: 5명씩, Time: 0.03 ms, Memory: 75 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181886. 5명씩/5명씩.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+
+class Solution {
+    public String[] solution(String[] names) {
+        
+        ArrayList <String> list=new ArrayList<>();
+        for(int i = 0; i < names.length; i++){
+            if(i%5 == 0)
+                list.add(names[i]);
+        }
+        String[] answer = new String[list.size()];
+        for(int i = 0; i < list.size(); i++){
+            answer[i] = list.get(i);
+        }
+        return answer;
+    }
+}
