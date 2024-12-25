70ccce5d3e964f417cf899bae8438fc29bb252d1
tngns1129 <57630359+tngns1129@users.noreply.github.com>
2024-12-25 23:19:30 +0900

    [Silver IV] Title: 체스판 다시 칠하기, Time: 184 ms, Memory: 17968 KB -BaekjoonHub

--- 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.java
+++ 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.java
@@ -13,20 +13,9 @@ public class Main {
         for(int i=0; i< board.length; i++){
             board[i] = sc.nextLine();
         }
-        char chess[][] = new char[8][8];
         char input[][] = new char[row][col];
-        int test=64;
-        int a=0;
-        char[][] nomalChess1 = {
-                {'W','B','W','B','W','B','W','B'},
-                {'B','W','B','W','B','W','B','W'},
-                {'W','B','W','B','W','B','W','B'},
-                {'B','W','B','W','B','W','B','W'},
-                {'W','B','W','B','W','B','W','B'},
-                {'B','W','B','W','B','W','B','W'},
-                {'W','B','W','B','W','B','W','B'},
-                {'B','W','B','W','B','W','B','W'}
-        };
+        int answer=64;
+        int fixNum=0;
 
         for(int i=0; i<board.length; i++){
             input[i] = board[i].toCharArray();
@@ -36,43 +25,23 @@ public class Main {
             for(int j=0; j<input[0].length-7; j++){
                 for(int x=0; x<8; x++){
                     for(int y=0; y<8; y++){
-                        chess[x][y] = input[i+x][j+y];
-                        if(nomalChess1[x][y] == input[i+x][j+y]){
-                            a++;
+                        if((x+y)%2 == 0){
+                            if(input[i+x][j+y] == 'W'){
+                                fixNum++;
+                            }
+                        }else{
+                            if(input[i+x][j+y] == 'B'){
+                                fixNum++;
+                            }
                         }
                     }
                 }
-                if(test>Math.min(a,64-a)){
-                    test = Math.min(a,64-a);
-                    
+                if(answer>Math.min(fixNum,64-fixNum)){
+                    answer = Math.min(fixNum,64-fixNum);
                 }
-                a=0;
+                fixNum=0;
             }
         }
-
-        System.out.println(test);
-    }
-
-    static int numOfPaint(char chess[][]){
-        int a=0;
-        char[][] nomalChess1 = {
-                {'W','B','W','B','W','B','W','B'},
-                {'B','W','B','W','B','W','B','W'},
-                {'W','B','W','B','W','B','W','B'},
-                {'B','W','B','W','B','W','B','W'},
-                {'W','B','W','B','W','B','W','B'},
-                {'B','W','B','W','B','W','B','W'},
-                {'W','B','W','B','W','B','W','B'},
-                {'B','W','B','W','B','W','B','W'}
-        };
-
-        for(int i=0; i< chess.length; i++) {
-            for (int j = 0; j < chess[0].length; j++) {
-                if(chess[i][j] != nomalChess1[i][j]){
-                    a++;
-                }
-            }
-        }
-        return Math.min(a, 64-a);
+        System.out.println(answer);
     }
 }
